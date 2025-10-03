52355c5bb89c56f897f38867da5310d7756abcff
hiratahir29 <ayaz.gillani@noborderz.com>
2025-10-03 17:47:58 -0400

    FEPage-styling

---
+++ components/events/EventSearch.js
@@ -0,0 +1,12 @@
+import { useRef } from 'react';
+import Button from '../ui/Button';
+import styles from './EventSearch.module.css';
+import { useRouter } from 'next/router';
+export default function EventSearch(props){
+
+    return(
+        <form >
+
+        </form>
+    )
+}
