3ae7e6771aa3fcbdd6bb7675bd54ccad21893a8b
hiratahir29 <hirab.299@gmail.com>
2025-10-07 14:25:36 -0400

    showed all events in row

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,16 @@
+import EventList from "@/components/events/EventList";
+import { getAllEvents } from "@/dummy-data";
+
+
+
+
 export default function AllEventsPage() {
+  const eventlist=getAllEvents();
     return (
-        <div>
-          <h1>All Event Page</h1>
+        <div style={{textAlign:"center",fontFamily:"cursive"}}>
+          
+          <h1>All Events Page</h1>
+          <EventList list={eventlist}/>
         </div>
       
     );
