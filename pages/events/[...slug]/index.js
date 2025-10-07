7e37a8b2000cd7b5b14d7bbdac8f926f353c5148
hiratahir29 <hirab.299@gmail.com>
2025-10-07 17:18:42 -0400

    implemented CSR through  SWRHook

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,31 +1,90 @@
 import EventList from "@/components/events/EventList";
-import { getFilteredEvents } from "@/dummy-data";
+import { getFilteredEvents } from "../../../helpers/api-util";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import { useState , useEffect} from "react";
 
-export default function FilteredEventsPage() {
+ function FilteredEventsPage(props) {
+
+  //client side rendering code
+  const [events,setEventsState]=useState();
   const r=useRouter();
-  const filteredData=r.query.slug;
-  if(!filteredData)
-  {
-    return <p className="center">Loading...</p>
-  }
-  const year=Number(filteredData[0]);
-  const month=Number(filteredData[1]);
-  if(isNaN(year) || isNaN(month))
-  {
-    return <p>Invalid Year or Month</p>
-  }
-
-  const filteredEvents=getFilteredEvents({year:year,month:month});
-  if(!filteredEvents || filteredEvents.length===0)
+  const paramData=r.query.slug;
+  const fetcher=(url)=>fetch(url).then(res=>res.json())
+  const {data,error}=useSWR("https://eventsapp-61f2e-default-rtdb.firebaseio.com/events.json",fetcher)
+  useEffect(()=>{
+    if(data)
     {
-      return <p>No events found for this check</p>
-    } 
-  return (
+      const events=[]
+        for(const key in data)
+        {
+            events.push({
+                id:key,
+                ...data[key]
+            })
+        }
+        setEventsState(events)
+    }
+  },[data])
+  
+  if(!events)
+  {
+    return <p>Loading...</p>
+  }
+  const year=Number(paramData[0]);
+  const month=Number(paramData[1]);
+
+  const filteredEvents=events.filter((event) => {
+  const eventDate = new Date(event.date);
+      return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+    });
+  if(isNaN(year)|| isNaN(month) || error)
+      {
+        return <p>Invalid year or month</p>
+      }
+
+  // if(props.hasError===true)
+  // {
+  //   return <p>Invalid year or month</p>
+  // }
+
+  // if(!props.filteredEvents||props.filteredEvents.length===0)
+  //   {
+  //     return <p>Event not Found</p>
+  //   }
+  if(!filteredEvents||filteredEvents.length===0)
+  {
+    return <p>Event not Found</p>
+  }
+    return (
+      
         <div style={{textAlign:"center",fontFamily:"cursive"}}>
-          <h1>Filtered Events Page</h1>
+          <h1>Filtered Events Page</h1> 
           <EventList list={filteredEvents}/>
         </div>
       
     );
   }
+
+  // export async function getServerSideProps(context){
+  //   const ym=context.params.slug
+  //   const year=Number(ym[0]);
+  //   const month=Number(ym[1]);
+  // if(isNaN(year)|| isNaN(month))
+  // {
+  //   return{
+  //     //notFound: true   go to 404 page
+  //     props:{            //but we have proper messaging for this type of error
+  //       hasError:true
+  //     }
+  //   }
+  // }
+  // const arr= await getFilteredEvents({year:year,month:month})
+
+  //   return{
+  //     props:{
+  //         filteredEvents: arr
+  //     }
+  //   }
+  // }
+  export default FilteredEventsPage;
