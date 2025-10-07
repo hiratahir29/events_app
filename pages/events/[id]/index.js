4a03933b6657813000d5b4ffbd79a785724ebb52
hiratahir29 <hirab.299@gmail.com>
2025-10-07 14:36:31 -0400

    implemented detail page

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -1,8 +1,27 @@
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import Event from "@/components/events/Event";
+import EventContent from "@/components/event-detail/event-content";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+
+
 export default function DetailedEventPage() {
+  const r=useRouter();
+  const event=getEventById(r.query.id);
+  if(!event){
+    return <p>No event found!</p>
+  }
+  else
+  {      
     return (
-        <div>
+        <div style={{textAlign:"center",fontFamily:"cursive"}}>
           <h1>Detailed Event Page</h1>
+          <EventSummary title={event.title}/>
+          <EventLogistics d={event.date} ad={event.location} i={event.image} t={event.title}/>
+          <EventContent><p>{event.description}</p></EventContent>
         </div>
       
     );
   }
+}
