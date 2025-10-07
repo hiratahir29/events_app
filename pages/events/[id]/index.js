8da5bedb175387a46983923789a0ba32609221b5
hiratahir29 <hirab.299@gmail.com>
2025-10-07 17:25:08 -0400

    implemented SSG and getStaticPaths

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -1,29 +1,49 @@
-import { useRouter } from "next/router";
-import { getEventById } from "@/dummy-data";
-import Event from "@/components/events/Event";
 import EventContent from "@/components/event-detail/event-content";
-import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getAllEvents, getEventById, getFeaturedEvents } from "../../../helpers/api-util";
 import Comments from "@/components/input/comments";
 
 
-export default function DetailedEventPage() {
-  const r=useRouter();
-  const event=getEventById(r.query.id);
-  if(!event){
-    return <p>No event found!</p>
-  }
-  else
-  {      
+function DetailedEventPage(props) {
+
+    const event=props.selectedEvent;
+    if(!event)
+    {
+      return <p>Event doesn't exists</p>
+    }
     return (
-        <div style={{textAlign:"center",fontFamily:"cursive"}}>
-          <h1>Detailed Event Page</h1>
+      
+        <div style={{textAlign:"center", fontFamily:"cursive"}}>
+          <h1>Event Detail Page</h1> 
           <EventSummary title={event.title}/>
-          <EventLogistics d={event.date} ad={event.location} i={event.image} t={event.title}/>
+          <EventLogistics t={event.title} d={event.date} ad={event.location} i={event.image}/>
           <EventContent><p>{event.description}</p></EventContent>
           <Comments eventId={event.id}/>
         </div>
       
     );
   }
-}
+
+
+  export async function getStaticProps(context){
+    const eid=context.params.id;
+    const event= await getEventById(eid);
+    return {
+      props:{
+        selectedEvent:event
+      }
+      // ,
+      // revalidate:30   //temporarily removed it cz of github pages
+    }
+  }
+
+  export async function getStaticPaths(){
+    const mostlyVisitedPages=await getFeaturedEvents();
+    const theirpaths=mostlyVisitedPages.map(obj=>({params:{id:obj.id}}));
+    return {
+      paths:theirpaths,
+      fallback:'blocking'
+    }
+  }
+  export default DetailedEventPage;
